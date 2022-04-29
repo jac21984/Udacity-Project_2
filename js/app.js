641e64829ad829d53bc9526011b800711fb004d7
jac21984 <jac21984@me.com>
2022-04-29 00:23:01 -0700

    Upload Site
    Uploading first version of site.

---
+++ js/app.js
@@ -0,0 +1,135 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+	//declare variable for section names
+	let sectionNames = [];
+	let sectionList = [];
+	let sectionTop = [];
+	let sectionBottom = [];
+	let sectionHeight = [];
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+	
+	//gets list of section titles
+	function getSectionListNames(){
+		sectionList = document.querySelectorAll('.landing__container');
+		for (let i = 0; i< sectionList.length; i++){
+			sectionNames[i] = sectionList[i].children[0].innerText;
+			sectionTop[i] = sectionList[i].parentElement.offsetTop;
+			sectionHeight[i] = sectionList[i].parentElement.offsetHeight;
+			sectionBottom[i] = sectionTop[i] + sectionHeight[i];
+			//alert(sectionNames[i]);
+		}
+		//window.alert(sectionList.length);
+		
+		populateMenu();
+	}
+	
+	//populates menu buttons with section name
+	function populateMenu(){
+		//create div for menu
+		const menuDiv = document.createElement('div');
+
+		for (let i = 0; i< sectionNames.length; i++){
+			const newButton = document.createElement('button');
+			newButton.setAttribute('id','btn ' + i);
+			newButton.setAttribute('class','inactive');
+			newButton.setAttribute('onClick','btnClicked(this.id)');
+			newButton.innerHTML = sectionNames[i];
+			
+			menuDiv.appendChild(newButton);
+		}
+		
+		//gets header id
+		const navListMenu = document.getElementById('navbar__list');
+		
+		navListMenu.appendChild(menuDiv);
+	}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+	function checkDOMloaded(){
+		document.addEventListener('DOMContentLoaded', getSectionListNames());
+		window.onscroll = function() {activeSection()};
+	}
+
+// Scroll to section on link click
+
+function btnClicked(clicked_id){
+	var btnIndex = parseInt(clicked_id.replace('btn ',''));
+	var scrollIndex = sectionTop[btnIndex] + (btnIndex * 55);
+	
+	window.scrollTo({
+		top: scrollIndex,
+		behavior: 'smooth'
+	});
+	
+}
+
+// Set sections as active
+
+function activeSection() {
+	//gets current page scroll position
+	let pageTop = document.body.scrollTop;
+	
+	for (let i = 0; i< sectionNames.length; i++){
+		//retrieved button ID
+		let btnTest = document.getElementById('btn ' + i);
+		
+		//tests if section on viewport and set active if true
+		if (pageTop >= (sectionTop[i]-300) && pageTop <= (sectionBottom[i]-300)) {
+			sectionList[i].parentElement.className = "active";
+			btnTest.className = 'active';
+		} else {
+			sectionList[i].parentElement.className = "inactive";
+			btnTest.className = 'inactive';
+		}
+	}
+}
+
+
